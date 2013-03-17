913e0b8b098238f307b24b1d814f24d7cd78725e
drusk <drusk@uvic.ca>
2013-03-16 18:06:14 -0700

    Added mixture of Gaussians background subtractor for segmentation.

--- segment.py
+++ segment.py
@@ -33,3 +33,19 @@ class MovingAverageBackgroundSubtractor(object):
                                      cv2.THRESH_BINARY)
         return segmented
 
+
+class MixtureOfGaussiansBackgroundSubtractor(object):
+    
+    def __init__(self):
+        history = 1000 # any value > 0; default 200
+        num_gaussians = 5 # can be from 1 - 8; default 5
+        background_ratio = 0.8 # any value > 0 and < 1; default 0.7
+        noise_sigma = 0.5 # any value > 0; default 15
+        self.background_subtractor = cv2.BackgroundSubtractorMOG(history,
+                                            num_gaussians, background_ratio,
+                                            noise_sigma)
+
+    def segment(self, current_image):
+        grayscale = cv2.cvtColor(current_image, cv2.COLOR_BGR2GRAY)
+        learning_rate = -1 # > 0 and < 1; -1 for automatically calculated value
+        return self.background_subtractor.apply(grayscale, None, learning_rate)
